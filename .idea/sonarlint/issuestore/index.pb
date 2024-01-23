1f3d7aac5ca39956169afc190ed214ac5f3678ea
Heimy Dias <heimysantana@hotmail.com>
2024-01-23 20:20:01 -0300

    first class: position

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -4,3 +4,5 @@ L
 :
 
 .gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+K
+src/boardgame/Position.java,6\1\61f04ddcd53c8e2006fef8201e5cd678001b0dc4
--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
+    public static void main(String[] args) {
+
+        Position position = new Position(36, 200);
+
+        System.out.println(position);
+    }
 }
